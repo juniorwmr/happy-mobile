016f87cb014044fe9c3a875c563f194e4694b666
Washington Muniz <juniorripardo@gmail.com>
2020-10-23 22:26:06 -0500

    onboarding finished

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import 'react-native-gesture-handler';
 import { useFonts } from 'expo-font';
 import {
@@ -7,9 +9,28 @@ import {
   Nunito_800ExtraBold,
 } from '@expo-google-fonts/nunito';
 
+import HappyOnboarding from './src/components/Onboarding';
 import Routes from './src/routes';
 
 export default function App() {
+  const [onboarding, setOnboarding] = useState<boolean>(false);
+  async function onDoneFunction() {
+    setOnboarding(true);
+    await AsyncStorage.setItem('onboarding', 'done');
+  }
+
+  useEffect(() => {
+    async function getOnboardingValue() {
+      const onboardingvalue = await AsyncStorage.getItem('onboarding');
+      if (onboardingvalue) {
+        setOnboarding(true);
+      } else {
+        setOnboarding(false);
+      }
+    }
+    getOnboardingValue();
+  }, []);
+
   const [fontsLoaded] = useFonts({
     Nunito_600SemiBold,
     Nunito_700Bold,
@@ -19,6 +40,10 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   } else {
-    return <Routes />;
+    return onboarding ? (
+      <Routes />
+    ) : (
+      <HappyOnboarding onDoneFunction={onDoneFunction} />
+    );
   }
 }
